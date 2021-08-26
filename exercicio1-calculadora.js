e55c99d047f47df3500006c072e10edfbcc87e48
igor <igor.rafael.briano@gmail.com>
2021-08-25 22:00:11 -0300

    Finalizado exercicio 1

--- exercicio1-calculadora.js
+++ exercicio1-calculadora.js
@@ -1,3 +1,17 @@
-let a = 5;
+let a = 4;
+let b = 2;
 
-console.log(a);
+console.log("Adicao");
+console.log(a+b);
+
+console.log("Subtracao");
+console.log(a-b);
+
+console.log("Multiplicacao");
+console.log(a*b);
+
+console.log("Divisao");
+console.log(a/b);
+
+console.log("Modulo");
+console.log(a%b);
