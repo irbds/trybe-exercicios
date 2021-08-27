c6569fec9e7e71bf2cb8bc1b0bc063aabcc202ed
igor <igor.rafael.briano@gmail.com>
2021-08-27 18:47:45 -0300

    pequena organizacao

--- exercicio01-Piramide.js
+++ exercicio01-Piramide.js
@@ -1,6 +1,8 @@
 let n = 5;
 let Base = [];
 
+//parte 1 do exercicio, criacao do quadrado
+if(true){
 //cria a base da piramide
 for(let index = 0; index<n; index += 1 ){
   Base.push("*");
@@ -9,4 +11,5 @@ for(let index = 0; index<n; index += 1 ){
 //cria a altura da piramide
 for(let index = 0; index<n; index += 1){
 console.log(Base);
+}
 }
