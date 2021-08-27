ea94a24410ca418af5acd4874e1c9b32ee22a938
igor <igor.rafael.briano@gmail.com>
2021-08-27 19:46:23 -0300

    finalizado parte 3

--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
 # trybe-exercicios
 
 #### Treino de logica de programação
+
+
+1-cria uma linha com n character
+  1.1-se a coluna atual for a n-1 cria um "*" se não cria um " "
--- exercicio01-Piramide.js
+++ exercicio01-Piramide.js
@@ -9,15 +9,30 @@ let Base = [];
 
 // //cria a altura da piramide
 // for(let index = 0; index<n; index += 1){
-// console.log(Base);
+// console.log(Base.join(""));
 // }
 //// -------------------------------------------------------------------------
 
 
-//------------------------------parte 2-----------------------------------
-//piramide a esquerda
-for(let index = 0; index<n; index += 1 ){
-  Base.push("*");
-  console.log(Base);
+// //------------------------------parte 2-----------------------------------
+// //piramide a esquerda
+// for(let index = 0; index<n; index += 1 ){
+//   Base.push("*");
+//   console.log(Base.join(""));
+// }
+// //-------------------------------------------------------------------------
+
+//------------------------------parte 3-----------------------------------
+for(let linha = 0; linha<n; linha += 1){
+  for(let coluna = 0; coluna<n; coluna += 1 ){
+    if(coluna+1 >= n-linha){
+      // console.log(linha,coluna);
+      Base.push("*");
+    }else{
+      Base.push(" ");
+    }
+  }
+  console.log(Base.join(""));
+  Base = [];
 }
 //-------------------------------------------------------------------------
