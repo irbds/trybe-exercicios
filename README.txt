b2cec0653e393341ca60a72c8b07bf216806bf85
igor <igor.rafael.briano@gmail.com>
2021-08-27 18:37:46 -0300

    criado quadrado

--- README.txt
+++ README.txt
@@ -1,4 +1,3 @@
 # trybe-exercicios
-Todos exercicios da trybe separados por branch
 
 #### Treino de logica de programação
---
+++ exercicio01-Piramide.js
@@ -0,0 +1,12 @@
+let n = 5;
+let Base = [];
+
+//cria a base da piramide
+for(let index = 0; index<n; index += 1 ){
+  Base.push("*");
+}
+
+//cria a altura da piramide
+for(let index = 0; index<n; index += 1){
+console.log(Base);
+}
