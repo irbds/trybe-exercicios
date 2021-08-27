2a1fc90dc75b1f7a23ea9a026b9b1620223ddf25
igor <igor.rafael.briano@gmail.com>
2021-08-27 18:53:23 -0300

    parte 2 piramide a esquerda

--- exercicio01-Piramide.js
+++ exercicio01-Piramide.js
@@ -1,15 +1,23 @@
 let n = 5;
 let Base = [];
 
-//parte 1 do exercicio, criacao do quadrado
-if(true){
-//cria a base da piramide
+//// ------------------------------parte 1-----------------------------------
+// //cria a base da piramide
+// for(let index = 0; index<n; index += 1 ){
+//   Base.push("*");
+// }
+
+// //cria a altura da piramide
+// for(let index = 0; index<n; index += 1){
+// console.log(Base);
+// }
+//// -------------------------------------------------------------------------
+
+
+//------------------------------parte 2-----------------------------------
+//piramide a esquerda
 for(let index = 0; index<n; index += 1 ){
   Base.push("*");
+  console.log(Base);
 }
-
-//cria a altura da piramide
-for(let index = 0; index<n; index += 1){
-console.log(Base);
-}
-}
+//-------------------------------------------------------------------------
