c0647bf1b49774993055207669342d4eee24e205
igor <igor.rafael.briano@gmail.com>
2021-08-28 15:34:12 -0300

    finalizado exercicio 4

--- README.txt
+++ README.txt
@@ -3,5 +3,19 @@
 #### Treino de logica de programação
 
 
+--3°--
 1-cria uma linha com n character
-  1.1-se a coluna atual for a n-1 cria um "*" se não cria um " "
+  1.1-se a coluna da linha atual for a n-1 cria um "*" se não cria um " "
+2-pula uma linha e cria outra linha com n character
+  2.1-se a coluna da linha atual for a n-2 ou maior cria um "*" se não cria um " "
+
+--4°--
+
+1-Recebe um numero de * para a base da piramide
+2-Cria uma array apenas com os numeros impares para a estrutura da piramide
+3-Pega o valor da base da piramide, divide por 2
+4-Armazena o valor da divisao em uma variavel chamada ValorEspaco
+6-Cria uma array chamada linhas para armazenar o valor de cada linha
+5-Cria a primeira linha com ValorEspaco de espacos e adiciona um "*" no final
+6-Pula para proxima linha
+7-Pega o numero impar anterior ao numero da base e repete os passos ate o numero pego ser maior que a base
--- exercicio01-Piramide.js
+++ exercicio01-Piramide.js
@@ -1,7 +1,6 @@
-let n = 5;
-let Base = [];
-
 //// ------------------------------parte 1-----------------------------------
+// let n = 5;
+// let Base = [];
 // //cria a base da piramide
 // for(let index = 0; index<n; index += 1 ){
 //   Base.push("*");
@@ -15,6 +14,8 @@ let Base = [];
 
 
 // //------------------------------parte 2-----------------------------------
+// let n = 5;
+// let Base = [];
 // //piramide a esquerda
 // for(let index = 0; index<n; index += 1 ){
 //   Base.push("*");
@@ -22,17 +23,50 @@ let Base = [];
 // }
 // //-------------------------------------------------------------------------
 
-//------------------------------parte 3-----------------------------------
-for(let linha = 0; linha<n; linha += 1){
-  for(let coluna = 0; coluna<n; coluna += 1 ){
-    if(coluna+1 >= n-linha){
-      // console.log(linha,coluna);
-      Base.push("*");
-    }else{
-      Base.push(" ");
-    }
+// //------------------------------parte 3-----------------------------------
+// let n = 5;
+// let Base = [];
+// for(let linha = 0; linha<n; linha += 1){
+//   for(let coluna = 0; coluna<n; coluna += 1 ){
+//     if(coluna+1 >= n-linha){
+//       // console.log(linha,coluna);
+//       Base.push("*");
+//     }else{
+//       Base.push(" ");
+//     }
+//   }
+//   console.log(Base.join(""));
+//   Base = [];
+// }
+// //-------------------------------------------------------------------------
+
+//------------------------------parte 4-----------------------------------
+let n = 50;
+let BaseImpar = [];
+
+for(let index = 0; index<=n; index+=1){
+  if(index%2 == 1){
+    BaseImpar.push(index);
   }
-  console.log(Base.join(""));
-  Base = [];
+}
+
+let altura = BaseImpar.length-1;
+let linhaPiramide = [];
+
+for(let linhas = 0; linhas <= altura; linhas += 1){
+
+  //Cria espacos
+  let div = (BaseImpar[(altura)-linhas])/2
+  for(let espaco = 0; espaco<div; espaco++){
+    linhaPiramide.push(" ");
+  }
+
+  //Cria asterisco
+  for(let asterisco = 0; asterisco < BaseImpar[linhas]; asterisco += 1){
+    linhaPiramide.push("*");
+  }
+
+  console.log(linhaPiramide.join(""));
+  linhaPiramide=[];
 }
 //-------------------------------------------------------------------------
