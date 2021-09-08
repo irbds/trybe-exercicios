07208b4cce436c06c31805b20bd80515f7b632af
igor rafael <igor.rafael.briano@gmail.com>
2021-09-08 15:56:24 -0300

    finalizado quarta parte do exercicio do dia

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ console.log('bem vinda, ' + info['personagem']);
 //parte 2 - inserir novo objeto e imprimi-lo
 info['recorrente'] = 'Sim';
 console.log(info);
+
+//parte 3 - Faça um for/in que mostre todas as chaves do objeto. 
+for(let chaves in info) {
+  console.log(chaves);
+}
