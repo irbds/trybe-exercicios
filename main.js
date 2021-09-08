055fdf8025b3962be8f1cea66474dab156bc6348
igor rafael <igor.rafael.briano@gmail.com>
2021-09-08 15:54:56 -0300

    finalizado segunda  parte do exercicio do dia

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+//parte 1
 console.log('bem vinda, ' + info['personagem']);
+
+//parte 2 - inserir novo objeto e imprimi-lo
+info['recorrente'] = 'Sim';
+console.log(info);
