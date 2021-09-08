d0458da11608e270410bf8d3a9665046510bf7b5
igor rafael <igor.rafael.briano@gmail.com>
2021-09-08 16:21:03 -0300

    finalizado quinta parte do exercicio do dia

--- main.js
+++ main.js
@@ -24,3 +24,20 @@ console.log('');
 for(let chavesValor in info) {
   console.log(info[chavesValor]);
 }
+
+//parte 5 -Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+console.log('');
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for(let valour in info) {
+  if(valour === 'recorrente' && info['recorrente'] === 'Sim' && info2['recorrente'] === 'Sim') {
+    console.log('Ambos recorrentes');
+  }else {
+    console.log(info[valour] + ' e ' + info2[valour]);
+  }
+}
