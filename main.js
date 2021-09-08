95bc2f5ec4701695472be19bd7f4d477f2f104d9
igor rafael <igor.rafael.briano@gmail.com>
2021-09-08 16:05:14 -0300

    finalizado quinta parte do exercicio do dia

--- main.js
+++ main.js
@@ -5,13 +5,22 @@ let info = {
 };
 
 //parte 1
+console.log('');
 console.log('bem vinda, ' + info['personagem']);
 
 //parte 2 - inserir novo objeto e imprimi-lo
+console.log('');
 info['recorrente'] = 'Sim';
 console.log(info);
 
 //parte 3 - Faça um for/in que mostre todas as chaves do objeto. 
+console.log('');
 for(let chaves in info) {
   console.log(chaves);
+}
+
+//parte 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+console.log('');
+for(let chavesValor in info) {
+  console.log(info[chavesValor]);
 }
