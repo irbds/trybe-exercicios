02d8f20facda49abb47032eca10cbd4f0a31059b
igor <igor.rafael.briano@gmail.com>
2021-10-06 20:16:11 -0300

    finalizado exercicio 3

--- main.js
+++ main.js
@@ -69,4 +69,25 @@ const NameGenAutor = books.map((book) => {
   return `${book.name} - ${book.genre} - ${book.author.name}`;
 })
 
-console.log(NameGenAutor);
+// console.log(NameGenAutor);
+
+// Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+const AgeBooks = books.map((book) => {
+  return {
+    age : book.releaseYear - book.author.birthYear,
+    author : book.author.name
+  }
+});
+
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+const FicFan = books.filter((book) =>
+  ((book.genre === 'Fantasia' || book.genre === 'Ficção Científica') ? book : '')
+);
+  
+console.log(FicFan);
+
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
