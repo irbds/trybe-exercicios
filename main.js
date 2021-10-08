6ed80716bded4cf71727fd7864010a3d423b93d1
igor <igor.rafael.briano@gmail.com>
2021-10-08 18:26:21 -0300

    realizado algumas alteracoes de leitura

--- main.js
+++ main.js
@@ -118,17 +118,10 @@ const letterAaTotal = names.reduce((acc, curr) => {
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
-
 const newArray = students.map((student, index) => {
   return {
     name : student,
     average : (grades[index].reduce((acc, curr) => acc += curr, 0)) / grades[index].length
   }
 })
-
 console.log(newArray);
