6a4823d9f3a8015d602b008d13fff9f926b549f7
igor <igor.rafael.briano@gmail.com>
2021-10-15 20:30:56 -0300

    finalizado exercicios 1 a 4

---
+++ main.js
@@ -0,0 +1,111 @@
+// ===============================exercicio-1=================================
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// console.log(planetDistanceFromSun(venus)); // B
+// console.log(planetDistanceFromSun(jupiter)); // C
+
+// ===============================exercicio-2=================================
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// ===============================exercicio-3=================================
+
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   setTimeout(() => console.log("Returned planet: ", mars), 4000);
+// };
+
+// getPlanet(); // imprime Marte depois de 4 segundos
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+// `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   return planetDistanceFromSun(mars);
+// };
+
+// console.log(getPlanet()); // imprime Marte depois de 4 segundos
+
+// ===============================exercicio-4=================================
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// const sendMarsTemperature = (callback) => {
+//   setTimeout(() => console.log(`Mars temperature is: ${callback()} degree Celsius`), messageDelay());
+// };
+
+// sendMarsTemperature(getMarsTemperature);
+
+// ===============================exercicio-5=================================
